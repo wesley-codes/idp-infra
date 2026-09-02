6c6d4c6e0c8ea5835d06f258df52540a337e3fae
Meka <ezemikey12@gmail.com>
2026-09-02 16:11:33 -0400

    Debug

--- environments/global/github-oidc.tf
+++ environments/global/github-oidc.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "github_actions" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = { "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com" }
-        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:wesley-codes/idp-infra:*" }
+        StringLike   = { "token.actions.githubusercontent.com:sub" = "repo:wesley-codes/idp-infra@*:*" }
 
       }
     }]
